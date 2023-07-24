23c7e637632fde749e5390ce22d9aaa14863defd
Your Name <you@example.com>
2023-07-24 12:51:23 +0200

    Exercise 3 & 4

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,19 @@
 "use strict";
-// INTERFACES
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var _a, _b, _c;
+//Array to store joke's reports & score value
+const reportJokes = [];
+let scoreValue;
 // fetchJoke()
+(_a = document.getElementById("nextButton")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", fetchJoke);
 function fetchJoke() {
     const urlJokes = "https://icanhazdadjoke.com/";
     fetch(urlJokes, {
@@ -14,3 +27,59 @@ function fetchJoke() {
         $jokesText.innerHTML = res.joke;
     });
 }
+// Score buttons to appear
+(_b = document
+    .getElementById("nextButton")) === null || _b === void 0 ? void 0 : _b.addEventListener("click", displayScoreButtons);
+function displayScoreButtons() {
+    const $scoreButtons = (document.getElementById("score-options"));
+    $scoreButtons.innerHTML = `
+    <button onclick="collectScore(1)" class="score-btn"><img src="/public/css/images/emoji1.png" alt="emoji 1"></button>
+    <button onclick="collectScore(2)" class="score-btn"><img src="/public/css/images/emoji2.png" alt="emoji 2"></button>
+    <button onclick="collectScore(3)" class="score-btn"><img src="/public/css/images/emoji3.png" alt="emoji 3"></button>
+  `;
+}
+// Select score option
+function collectScore(score) {
+    scoreValue = score;
+}
+// Save on the reportJokes[] array the corresponding object
+(_c = document.getElementById("nextButton")) === null || _c === void 0 ? void 0 : _c.addEventListener("click", saveReport);
+function saveReport() {
+    const $jokesText = (document.getElementById("jokes-text"));
+    const currentDate = new Date().toISOString();
+    let jokeReported;
+    if (typeof scoreValue === "number") {
+        jokeReported = {
+            joke: $jokesText.textContent,
+            score: scoreValue,
+            date: currentDate,
+        };
+        reportJokes.push(jokeReported);
+        console.log(reportJokes);
+    }
+    scoreValue = undefined;
+}
+//Weather API
+function fetchWeather() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const urlWeather = "https://weatherapi-com.p.rapidapi.com/current.json?q=Barcelona";
+        const options = {
+            method: "GET",
+            headers: {
+                "X-RapidAPI-Key": "0d030e417cmsh969e291855f2103p1f52e0jsn26371d57982e",
+                "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com",
+            },
+        };
+        try {
+            const response = yield fetch(urlWeather, options);
+            const result = yield response.json();
+            const $weatherText = (document.getElementById("weather"));
+            $weatherText.innerHTML = `${result.location.name} - ${result.current.condition.text}`;
+            console.log(result);
+        }
+        catch (error) {
+            console.error(error);
+        }
+    });
+}
+fetchWeather();
--- ts/main.ts
+++ ts/main.ts
@@ -1,12 +1,28 @@
 // INTERFACES
-
 interface joke {
   id: string;
   joke: string;
   status: number;
 }
 
+interface weather {
+  location: {
+    name: string;
+  };
+  current: {
+    condition: {
+      text: string;
+      icon: string;
+    };
+  };
+}
+
+//Array to store joke's reports & score value
+const reportJokes: object[] = [];
+let scoreValue: number | undefined;
+
 // fetchJoke()
+document.getElementById("nextButton")?.addEventListener("click", fetchJoke);
 
 function fetchJoke(): void {
   const urlJokes: string = "https://icanhazdadjoke.com/";
@@ -24,3 +40,72 @@ function fetchJoke(): void {
       $jokesText.innerHTML = res.joke;
     });
 }
+
+// Score buttons to appear
+document
+  .getElementById("nextButton")
+  ?.addEventListener("click", displayScoreButtons);
+function displayScoreButtons(): void {
+  const $scoreButtons: HTMLElement = <HTMLElement>(
+    document.getElementById("score-options")
+  );
+
+  $scoreButtons.innerHTML = `
+    <button onclick="collectScore(1)" class="score-btn"><img src="/public/css/images/emoji1.png" alt="emoji 1"></button>
+    <button onclick="collectScore(2)" class="score-btn"><img src="/public/css/images/emoji2.png" alt="emoji 2"></button>
+    <button onclick="collectScore(3)" class="score-btn"><img src="/public/css/images/emoji3.png" alt="emoji 3"></button>
+  `;
+}
+
+// Select score option
+function collectScore(score: number): void {
+  scoreValue = score;
+}
+
+// Save on the reportJokes[] array the corresponding object
+document.getElementById("nextButton")?.addEventListener("click", saveReport);
+function saveReport(): void {
+  const $jokesText: HTMLElement = <HTMLElement>(
+    document.getElementById("jokes-text")
+  );
+  const currentDate: string = new Date().toISOString();
+  let jokeReported: object;
+
+  if (typeof scoreValue === "number") {
+    jokeReported = {
+      joke: $jokesText.textContent,
+      score: scoreValue,
+      date: currentDate,
+    };
+    reportJokes.push(jokeReported);
+    console.log(reportJokes);
+  }
+  scoreValue = undefined;
+}
+
+//Weather API
+async function fetchWeather(): Promise<void> {
+  const urlWeather: string =
+    "https://weatherapi-com.p.rapidapi.com/current.json?q=Barcelona";
+  const options: object = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "0d030e417cmsh969e291855f2103p1f52e0jsn26371d57982e",
+      "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com",
+    },
+  };
+
+  try {
+    const response: Response = await fetch(urlWeather, options);
+    const result: weather = await response.json();
+
+    const $weatherText: HTMLElement = <HTMLElement>(
+      document.getElementById("weather")
+    );
+    $weatherText.innerHTML = `${result.location.name} - ${result.current.condition.text}`;
+    console.log(result);
+  } catch (error) {
+    console.error(error);
+  }
+}
+fetchWeather();
